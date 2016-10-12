e837b87893b73b7c259c986214efe32485350954
王德正 <Tom@dezhengdeMacBook-Pro.local>
2016-10-11 23:07:38 -0500

    switch successfully between different views

---
+++ RidingMechanic/ActivationViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActivationViewController.h
+//  RidingMechanic
+//
+//  Created by 王德正  on 11/10/2016.
+//  Copyright © 2016 Dezheng Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivationViewController : UIViewController
+
+@end
---
+++ RidingMechanic/ActivationViewController.m
@@ -0,0 +1,22 @@
+//
+//  ActivationViewController.m
+//  RidingMechanic
+//
+//  Created by 王德正  on 11/10/2016.
+//  Copyright © 2016 Dezheng Wang. All rights reserved.
+//
+
+#import "ActivationViewController.h"
+
+@interface ActivationViewController ()
+
+@end
+
+@implementation ActivationViewController
+
+-(IBAction) backToRegister:(id)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+@end
