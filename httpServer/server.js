1fdc5acd04016ff85fc00151f3821ba8c2d07fd6
王德正 <Tom@dezhengdembp.intra.uwlax.edu>
2016-11-16 13:58:22 -0600

    upload server file

---
+++ httpServer/server.js
@@ -0,0 +1,47 @@
+var http= require("http");
+var settings=require("settings");
+var table=require("table");
+var httpMsg=require("httpMsg")
+
+http.createServer(function(req, resp){
+    switch(req.method){
+        case "GET":
+            if(req.url==="/"){
+                httpMsg.showHome(req,resp);
+            }else if(req.url==="/carmodels"){
+                table.getCarList(req,resp);
+             }else if(req.url==="/userInfo"){
+                table.getUserList(req,resp);
+            }else{
+                httpMsg.showHome(req,resp);
+            }
+            break;
+        case "POST":
+            if(req.url==="/userInfo"){
+                var reqBody='';
+                req.on("data",function(data){
+                    reqBody+=data;
+                    if(reqBody.length>1e7)//10mb
+                    {
+                        httpMsg.show413(req,resp);
+                    }
+                });
+                req.on("end",function(){
+                    table.addUser(req,resp,reqBody);
+                });
+
+            }else{
+                httpMsg.show404(req,resp);
+            }
+            break;
+        case "PUT":
+            break;
+        case "DELETE":
+            break;  
+        default :
+            httpMsg.show405(req,resp);
+            break;                                
+    }
+}).listen(settings.webPort,function(){
+    console.log("Start listening at:"+ settings.webPort);
+});
