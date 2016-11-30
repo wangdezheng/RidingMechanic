71256433d37fa477200612d3c15a9a65835451a9
王德正 <Tom@dezhengdeMacBook-Pro.local>
2016-11-29 20:46:02 -0600

    update

---
+++ RidingMechanic/SettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.h
+//  RidingMechanic
+//
+//  Created by 王德正  on 11/29/16.
+//  Copyright © 2016 Dezheng Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableViewController : UITableViewController
+
+@end
---
+++ RidingMechanic/SettingsTableViewController.m
@@ -0,0 +1,133 @@
+//
+//  SettingsTableViewController.m
+//  RidingMechanic
+//
+//  Created by 王德正  on 11/29/16.
+//  Copyright © 2016 Dezheng Wang. All rights reserved.
+//
+
+#import "SettingsTableViewController.h"
+
+@interface SettingsTableViewController ()
+
+@end
+
+@implementation SettingsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 3;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSInteger rows=0;
+    if(section==0){
+        rows=1;
+    }else if(section==1){
+        rows=2;
+    }else{
+        rows=2;
+    }
+    return rows;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
+    switch (indexPath.section)
+    {
+        case 0:
+            cell.textLabel.text =@"1";
+            break;
+        case 1:
+            cell.textLabel.text =@"2";
+            break;
+        case 2:
+            cell.textLabel.text =@"3";
+            break;
+        default:
+            break;
+    }
+    return cell;
+}
+
+- (NSString *) tableView: (UITableView *) tableView titleForHeaderInSection: (NSInteger) section {
+    NSString *sectionName;
+    switch (section)
+    {
+        case 0:
+            sectionName =@"Account settings";
+            break;
+        case 1:
+            sectionName =@"Vehicle settings";
+            break;
+        case 2:
+            sectionName =@"Alarm settings";
+            break;
+        default:
+            break;
+    }
+    return sectionName;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
