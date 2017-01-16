ddc76fc049e6d1804ae1282d87347a994893ada0
王德正 <Tom@dezhengdeMacBook-Pro.local>
2017-01-16 15:35:52 -0600

    update

--- RidingMechanic/HUDViewController.m
+++ RidingMechanic/HUDViewController.m
@@ -10,6 +10,7 @@
 
 @interface HUDViewController ()
 @property (strong, nonatomic) IBOutlet UIView *tabBarView;
+@property (strong, nonatomic) IBOutlet UIView *titleBarView;
 
 @end
 
@@ -28,30 +29,45 @@
 
 -(IBAction)singleTap:(id)sender
 {
-    CAAnimation *animation=[self animationForTabBarView:self.tabBarView];
-    [self.tabBarView.layer addAnimation:animation forKey:@"moveTabBar"];
+    CAAnimation *animationForTitle=[self animationForTabBarView:self.titleBarView];
+    [self.titleBarView.layer addAnimation:animationForTitle forKey:@"moveTitleBar"];
+    if(self.titleBarView.center.y<0){
+        self.titleBarView.center=CGPointMake(self.titleBarView.center.x, self.titleBarView.center.y+120);
+    }else{
+        self.titleBarView.center=CGPointMake(self.titleBarView.center.x, self.titleBarView.center.y-120);
+    }
+
+    CAAnimation *animationForTab=[self animationForTabBarView:self.tabBarView];
+    [self.tabBarView.layer addAnimation:animationForTab forKey:@"moveTabBar"];
 
     if(self.tabBarView.center.y<self.view.frame.size.height){
         self.tabBarView.center=CGPointMake(self.tabBarView.center.x, self.tabBarView.center.y+60);
     }else{
         self.tabBarView.center=CGPointMake(self.tabBarView.center.x, self.tabBarView.center.y-60);
     }
+    
     [self.view setNeedsDisplay];
+}
+
+-(CAAnimation *) animationForTitleBarView:(UIView *) titleBarView
+{
+    CABasicAnimation * positionAnimation = [CABasicAnimation animationWithKeyPath: @"position"];
+    positionAnimation.fromValue=[NSValue valueWithCGPoint:titleBarView.center];
+
+    positionAnimation.duration=0.5;
+    
+    return positionAnimation;
 }
 
 -(CAAnimation *) animationForTabBarView:(UIView *) tabBarView
 {
     CABasicAnimation * positionAnimation = [CABasicAnimation animationWithKeyPath: @"position"];
-    positionAnimation.fromValue=[NSValue valueWithCGPoint:tabBarView.center];
-    if(tabBarView.center.y<self.view.frame.size.height){
-        positionAnimation.toValue=[NSValue valueWithCGPoint:CGPointMake(tabBarView.center.x, tabBarView.center.y+60)];
-    }else{
-        positionAnimation.toValue=[NSValue valueWithCGPoint:CGPointMake(tabBarView.center.x, tabBarView.center.y-60)];
-    }
+
     positionAnimation.duration=0.5;
     
     return positionAnimation;
 }
+
 
 
 @end
---
+++ RidingMechanic/ListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListViewController.h
+//  RidingMechanic
+//
+//  Created by 王德正  on 1/16/17.
+//  Copyright © 2017 Dezheng Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListViewController : UIViewController
+
+@end
